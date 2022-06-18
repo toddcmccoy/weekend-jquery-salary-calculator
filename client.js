7831fdd8c7a054b0040cf49eebcaee99bad23d4c
Todd McCoy <tcmccoy@gmail.com>
2022-06-18 10:14:40 -0500

    edited CSS and html

--- client.js
+++ client.js
@@ -4,3 +4,9 @@ function onReady(){
     console.log('JS has arrived');
     console.log('jQuery is here too');
 }
+// create a function to collect inputs
+//append input info to the table
+
+//create a function that takes the annual summary and calculates the monthly expense
+
+//create a function that removes an employee from the table
