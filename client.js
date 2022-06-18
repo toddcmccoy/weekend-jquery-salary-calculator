a09a9d8af3257c5cc749e364b784dd7143685d64
Todd McCoy <tcmccoy@gmail.com>
2022-06-18 10:41:48 -0500

    created function in JS to get name inputs and add to DOM

--- client.js
+++ client.js
@@ -3,8 +3,21 @@ $(document).ready(onReady);
 function onReady(){
     console.log('JS has arrived');
     console.log('jQuery is here too');
+    $('#add-employee').on('click', addEmployee);
 }
+
+
+
 // create a function to collect inputs
+function addEmployee() {
+    console.log('clicked');
+    let firstname = $('#first-name').val();
+    let lastname = $('#last-name').val();
+    console.log(firstname);
+    console.log(lastname);
+    $('#employee-table').append(`<ti id="#f-name">${firstname}</li>`);
+    $('#employee-table').append(`<ti id="#l-name">${lastname}</li>`);
+}
 //append input info to the table
 
 //create a function that takes the annual summary and calculates the monthly expense
